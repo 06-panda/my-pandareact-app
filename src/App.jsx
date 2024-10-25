c73f5ae2fa9d78e3b7a173e79cdbe460079c332f
aanchaleeporn.sri <aanchaleeporn.sri@chonve.ac.th>
2024-10-25 15:57:53 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+function App() {
+  return (
+    <>
+    <div>
+     <h1>1</h1>
+     <h2>2</h2>   
+     <h3>3</h3>
+     <h4>4</h4>
+     <h5>5</h5>
+     </div>
+     <span>
+     <h3>3</h3>
+     <h4>4</h4>
+     <h5>5</h5>
+     </span>
+     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam rerum nemo cupiditate ipsum sunt. Tempore placeat nobis laudantium eveniet dolor, non fuga. Porro nostrum tempora provident mollitia et voluptatum incidunt!</p>
+    <span>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequatur, eum? Veritatis mollitia sapiente, repudiandae iusto facere a error sed ratione accusamus accusantium ad aliquid earum. Incidunt minima maiores asperiores perspiciatis!</span>
+    
+    
+    </>
+  )
+}
+
+export default App
+
